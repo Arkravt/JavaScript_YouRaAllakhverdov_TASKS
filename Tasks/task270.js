6adbcf8cdc56b1580256f09574046dd2adaa1f30
Kravtsev Artem <arkravt@gmail.com>
2024-01-30 16:37:03 +0400

    add task270.js

---
+++ Tasks/task270.js
@@ -0,0 +1,16 @@
+'use strcit'
+// Old solution 
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/russia`);
+// request.send();
+// request.addEventListener('load', function () {
+//    console.log(JSON.parse(this.responseText));
+// });
+
+
+// New solution
+const newRequest = fetch(`https://restcountries.com/v3.1/name/russia`)
+.then(response => response.json())
+.then(data => console.log(data[0]))
+.catch(e => console.log(`Метод catch выполняется в случае ошибки ${e}`))
+.finally(() => console.log('метод finally выполняется в любом случае ! '));
